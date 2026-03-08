0b5c0856b866fd2799c4e24dc7e5d20f7c219eaf
Surada Sri Anjaneya <suradasrianjaneya1819@gmail.com>
2026-03-08 11:05:45 +0530

    Enable data fetch using API

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = '8a93bf1f2da53fcd45d048d337967c63';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }else{
+        alert('Please enter a location');
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+            humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+        })
+        .catch(error => {
+            alert('Error fetching weather data. Couldn\'t find the city');
+        });
+}
